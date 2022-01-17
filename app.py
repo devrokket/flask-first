a61802d4d64ba819707523afc9ad502c0220e78c
offtheface <96538554+offtheface@users.noreply.github.com>
2022-01-17 23:35:05 +0900

    add shopping

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!!!!!'
+def root():
+    return 'Welcome'
+
+@app.route('/shopping')
+def shopping():
+    return 'All of themselves.'
 
