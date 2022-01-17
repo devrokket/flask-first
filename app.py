3ace58b25475c4f0041afefe31c2353ffe8042f0
offtheface <96538554+offtheface@users.noreply.github.com>
2022-01-18 00:35:48 +0900

    aws /answer inf_note_answer

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+from infinite_note.infinite_note import output_question
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def root():
     return 'Welcome'
 
+
 @app.route('/shopping')
 def shopping():
     return 'All of themselves.'
 
+
+@app.route('/answer')
+def inf_note_answer():
+    q = output_question()
+    return q
